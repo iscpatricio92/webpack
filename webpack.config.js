78b5f95efdd81595cabf56e4b2974482c33d98ca
Patricio <Patricio@C02T14VNG8WN.local>
2021-03-04 18:11:38 -0600

    minimizer css

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,14 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    filename: '[name].js',
     assetModuleFilename: 'assets/images/[hash][ext][query]'
   },
   resolve: {
@@ -55,7 +57,9 @@ module.exports = {
       template: './public/index.html',
       filename: './index.html'
     }),
-    new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin({
+      filename:'assets/css/[name].css'
+    }),
     new CopyPlugin({
       patterns: [
         {
@@ -64,5 +68,12 @@ module.exports = {
         }
       ]
     })
-  ]
+  ],
+  optimization:{
+    minimize:true,
+    minimizer:[
+      new CssMinimizerPlugin(),
+      new TerserPlugin()
+    ]
+  }
 }
